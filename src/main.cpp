84a568f183c51572387eac5230ab1df63ade9560
Christopher Rodriguez <cdr255@gmail.com>
2020-02-13 01:26:39 -0500

    Initial Card and Deck

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,38 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+class Card
+{
+public:
+  std::string get_name();
+  bool set_name(std::string);
+private:
+  std::string name = "";
+};
+
+class Deck
+{
+public:
+  std::string print_decklist();
+  bool add_card();
+  bool remove_card();
+private:
+  std::vector<std::reference_wrapper <Card>> list_of_cards;
+};
+
+std::string Card::get_name()
+{
+  return name;
+}
+
+bool Card::set_name(std::string newname)
+{
+    name = newname;
+    return true;
+}
+  
+
 
 int main(int argc, char * argv[])
 {
@@ -12,5 +46,10 @@ int main(int argc, char * argv[])
       std::cout << "\"" << argv[i] << "\"";
     }
   std::cout << ".\n";
+
+  Card test;
+  std::cout << test.get_name() << "\n";
+  test.set_name("Hello World!");
+  std::cout << test.get_name() << "\n";
 }
 
